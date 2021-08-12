8e42133e54b5595336854ae053dd475473da988e
Tiago Luz Cussate <TiagoC@Ortec.Com>
2021-08-12 13:04:11 -0300

    Exercicio concluido

--- brasil-com-p.js
+++ brasil-com-p.js
@@ -4,6 +4,15 @@ obj = {
     "palavras": process.argv.slice(4)
 }
 
-console.log(obj.primeiraLetra);
-console.log(obj.segundaLetra);
-console.log(obj.palavras);
+qtdPalavras = 0;
+
+for(let palavra of obj.palavras){
+    if(palavra[0] === obj.primeiraLetra && palavra[1] === obj.segundaLetra){
+        console.log(palavra);
+        qtdPalavras++;
+    }
+}
+
+if(qtdPalavras === 0){
+    console.log("NENHUMA");
+}
